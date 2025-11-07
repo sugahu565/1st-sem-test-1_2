74a65eba73e4cc69a79040367cb4097a5839019d
Katya Tupikova <k_tupikova00@mail.ru>
2025-11-07 16:41:26 +0300

    add check int x

--- src/bin_search.py
+++ src/bin_search.py
@@ -1,11 +1,16 @@
 def binSearch(xs: list[int], x: int):
+
+    if type(x) != int:
+        raise "x is not int"
+
     left, right = 0, len(xs) - 1
     while left < right:
         mid = (left + right) // 2
         if xs[mid] == x:
-            return mid
+            return xs[mid]
         if xs[mid] < x:
             left = mid + 1
         else:
             right = mid - 1
     return -1
+
