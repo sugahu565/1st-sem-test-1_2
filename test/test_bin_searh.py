0ebc0c9b6cddf6f5d78bae8c138e0b8ad0963047
Katya Tupikova <k_tupikova00@mail.ru>
2025-11-07 17:11:56 +0300

    add tests for bin_search

--- test/test_bin_searh.py
+++ test/test_bin_searh.py
@@ -11,3 +11,12 @@ def test_start():
 
 def test_not_in_list():
     assert binSearch([1, 2, 3, 4], 5) == -1
+
+
+def test_binary_lenght_middle_left():
+    assert binSearch([1, 2, 3, 4, 5, 6, 7, 8], 4) == 3
+
+
+def test_binary_lenght_middle_right():
+    assert binSearch([1, 2, 3, 4, 5, 6, 7, 8], 5) == 4
+
